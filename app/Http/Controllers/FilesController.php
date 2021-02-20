580ed7096d2fa9ec617c3ca0a59fc28872018114
alexchen <alexchen@abdstem.com>
2021-02-20 16:50:52 +0800

    优化页面

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Files;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class FilesController extends Controller
 {
---
+++ resources/views/form/_files_search.blade.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <div class="col-lg-12">
+        <form class="form-inline" action="{{ route('files.index') }}" method="get">
+            <div class="form-group">
+                <label class="sr-only" for="fileName">文件名</label>
+                <input type="text" class="form-control" id="fileName" name="file_name" placeholder="文件名">
+            </div>
+            <div class="form-group">
+                <label class="sr-only" for="extension">文件扩展名</label>
+                <input type="text" class="form-control" id="extension" name="file_extension" placeholder="文件扩展名">
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" id="repeat"> 仅查重
+                </label>
+            </div>
+            <button type="submit" class="btn btn-primary">确定</button>
+        </form>
+    </div>
+</div>
